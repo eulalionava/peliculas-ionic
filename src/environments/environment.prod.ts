6b7f78cd7b6e780d467fad3725d47c35d14e2eb0
eulalio <eulalio_cruz_nava@hotmail.com>
2021-02-13 23:25:22 -0600

    variables globales de la aplicacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url:'https://api.themoviedb.org/3',
+  apiKey:'8fec448c533621996b193b90eccf5e77',
+  imgPath:'https://image.tmdb.org/t/p'
 };
